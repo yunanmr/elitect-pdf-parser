2e08da3be937ddcd8b639a214e98a80ff62605c3
Yunan Mubarak Ramadani <yunanmr@yahoo.com>
2025-02-11 13:08:45 +0700

    init

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ use Spatie\PdfToText\Pdf;
 $str = Pdf::getText('EFI241107334-001.pdf');
 $data = [];
 
-preg_match('/Mode Code\s*:\s*(.*?)\s*Probe Type/', $str, $device);
-print_r($device);die;
+// preg_match('/Mode Code\s*:\s*(.*?)\s*Probe Type/', $str, $device);
+// print_r($device);die;
 
 preg_match_all('/(\d{2}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) (\d+\.\d+)/', $str, $logs);
 foreach ($logs[0] as $index => $match) {
